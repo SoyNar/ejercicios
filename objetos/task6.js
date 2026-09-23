7631bcc81514bef13a9a6d6339f65e0dba4553b1
SoyNar <narciris.tech@gmail.com>
2026-09-23 16:24:19 -0500

    update: actualizando task6

--- objetos/task6.js
+++ objetos/task6.js
@@ -1,5 +1,21 @@
 // ¡Escribe tu código debajo de esta línea!
+const argumentsC = process.argv.slice(3);
+const shoppingList = argumentsC.map((item) => {
+    const [article , amount ] = item.split(':');
+    return {
+        article: article,
+        ampunt:Number(amount)
+    }
+})
 
+class ShoppingLis {
+    constructor(items){
+        this.items = items;
 
+    }
+}
+
+const articles = new ShoppingLis(shoppingList);
+console.log(articles.items);
 
 // ¡Escribe tu código encima de esta línea!
