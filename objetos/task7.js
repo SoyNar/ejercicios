200448027180c8e206e76542f4369ff50a3be8ad
SoyNar <narciris.tech@gmail.com>
2026-09-23 16:36:25 -0500

    subiendo task7

--- objetos/task7.js
+++ objetos/task7.js
@@ -1,5 +1,22 @@
 // ¡Escribe tu código debajo de esta línea!
 
+function Auto(make, model, year, color, number, ofDoors, mileage, engine){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.number = number;
+  this.color = color;
+  this.ofDoors = ofDoors;
+  this.mileage = mileage;
+  this.engine = engine;
+  this.info = function(){
+    return `Información auto: marca ${make}, modelo ${model}, año ${year}, color ${color}, puertas ${ofDoors}, kilometraje ${mileage}, motor ${engine}`;  }
+}
 
+const [make, model, year, color, number, ofDoors, mileage, engine] = process.argv.slice(3);
+const newCar = new Auto(make, model, year, color, number, ofDoors, mileage, engine);
 
+const newCar2  = new Auto('Toyota', 'Corolla', 2022, 'Rojo', 'XYZ123', 4, 15000, 'combustion')
+// console.log(newCar);
+console.log(newCar2.info());
 // ¡Escribe tu código encima de esta línea!
